800e0a9b4714446bb824d2acc219aae89f161dfa
Andreas Stopat <dev@demonsgalore.de>
2019-07-21 11:31:42 +0200

    Add comments

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const mysql = require('mysql');
 
 const server = http.createServer((req, res) => {
 
+  console.log("Request URL", req.url);
+  
   // Path of file
   let filePath = '';
 
@@ -53,12 +55,19 @@ const server = http.createServer((req, res) => {
   // Read file
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      // Page not found
+      console.log(err);
+      
+      // File not found
       if (err.code === 'ENOENT') {
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
+        if (contentType !== 'text/html') {
+          res.writeHead(404, { 'Content-Type': contentType });
           res.end(content, 'utf8');
-        });
+        } else {
+          fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+            res.writeHead(404, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+          });
+        }
       } else {
         // Some server error
         res.writeHead(500);
